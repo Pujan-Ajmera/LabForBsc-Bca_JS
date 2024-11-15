a34ac489412a6f553608007b97a53924b3fe1cbe
Pujan-Ajmera <160005231+Pujan-Ajmera@users.noreply.github.com>
2024-11-15 20:48:40 +0530

    Add files via upload

---
+++ BSC-BSCIT Programs for lab/Lab24/eightynine.js
@@ -0,0 +1,13 @@
+export function add(a, b) {
+    return a + b;
+}
+
+export function subtract(a, b) {
+    return a - b;
+}
+
+
+// module.exports = {
+//     add: add,
+//     subtract: subtract
+// };
